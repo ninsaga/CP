e3e104954ed059e27406e83b7897e5e5620bdfcd
Nina <146337752+ninsaga@users.noreply.github.com>
2023-11-02 20:58:25 +0500

    Add files via upload

---
+++ prime_n.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int choice, starting_range, end_range, number_p;
+    while (true) {
+        cout << "Menu: \n 1. Find primes in range\n 2. Check if prime\n 3. Exit\n " << endl;
+        cout << "enter your choice " << endl;
+        cin >> choice;
+        if (choice == 1) {
+            cout << "enter 2 numbers indicating the start and end of the range" << endl;
+            cin >> starting_range >> end_range;
+            for (number_p = max(starting_range, 2); number_p <= end_range; number_p++) {
+                bool p = number_p > 1;
+                for (int i = 2; i * i <= number_p && p; i++) p = number_p % i;
+                if (p) cout << number_p << ' ';
+            }
+            cout << endl;
+        }
+        else if (choice == 2) {
+            cout << "enter the number u wanna check!" << endl;
+            cin >> number_p;
+            bool p = number_p > 1;
+            for (int i = 2; i * i <= number_p && p; i++) p = number_p % i;
+            cout << number_p << (p ? " is prime." : " is not prime.") << endl;
+        }
+        else if (choice == 3) {
+            cout << "Goodbye!" << endl;
+            break;
+        }
+        else cout << "Invalid choice. Select a valid option." << endl;
+    }
+    return 0;
+}
